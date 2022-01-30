1183abcf06c3f0445d6aaa12080e20c32406da59
ishelly1997 <ishelly1997@gmail.com>
2022-01-30 17:11:32 -0500

    replaced div with sidebar in html and created cards. js working on calling 3rd party api

--- assets/script.js
+++ assets/script.js
@@ -15,14 +15,15 @@ var apiUrl="https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04
 var btn = document.querySelector("#search-btn");
 var weatherCard = document.querySelector("#current-weather");
 var forecastCard = document.querySelector("#forecastContainer");
-//function identifyCity()
+
 
 //WHEN I search for a city
 //THEN I am presented with current and future conditions for that city and that city is added to the search history
-//function displayWeather
+
+
 //WHEN I view current weather conditions for that city
 //THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-//function currentWeather
+
 //WHEN I view the UV index
 //THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
 
@@ -32,15 +33,16 @@ var forecastCard = document.querySelector("#forecastContainer");
 //WHEN I click on a city in the search history
 //THEN I am again presented with current and future conditions for that city*/
 
-
-
-/*$(document).ready(function() {
+*$(document).ready(function() {
+    var cities = [];
     $(".search-btn").on("click", function() {
       var eventDetails =
       $("input").append(eventDetails);
       //console.log(eventDetails)
       var text = $(this).siblings(".form-control").val();
       localStorage.setItem(text);
-
+//function identifyCity()
+//function currentWeather
+//function displayWeather
 //Local Storage Items
-$("#search-city .form-control").val(localStorage.getItem("search-city"))});*/
+//$("#search-city .form-control").val(localStorage.getItem("search-city"))});*/
