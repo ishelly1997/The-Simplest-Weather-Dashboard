71bb1c6b6dac344da6a306680d684f91aec4d70e
ishelly1997 <ishelly1997@gmail.com>
2022-01-30 14:28:13 -0500

    create weather function issue

--- assets/script.js
+++ assets/script.js
@@ -8,13 +8,17 @@ var currentHumidty= $("#humidity");
 var currentWSpeed=$("#wind-speed");
 var currentUvindex= $("#UVindex");
 
+var storage=window.localStorage;
+
+var apiUrl="https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API key}"
+function identifyCity()
 
 //WHEN I search for a city
 //THEN I am presented with current and future conditions for that city and that city is added to the search history
-
+function displayWeather
 //WHEN I view current weather conditions for that city
 //THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-
+function currentWeather
 //WHEN I view the UV index
 //THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
 
