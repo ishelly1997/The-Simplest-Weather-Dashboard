654dc6587d193a73d1a94cc74d57802e10980795
ishelly1997 <ishelly1997@gmail.com>
2022-01-30 22:41:24 -0500

    running out of time will have to return to this assignment later for review

--- assets/script.js
+++ assets/script.js
@@ -1,48 +1,62 @@
 // API Key: 70bfb4227a0a636df084027cc1991ebc
-
-//GIVEN a weather dashboard with form inputs
 var city = "";
-var cities = [];
-var search = $("#search-city");
+var inputValue = $("#search-city");
 var searchBtn = $("#search-btn");
 var currentCity = $("#current-city");
 var name = $(".city-name")
 var currentTemperature = $("#temperature");
 var currentHumidty = $("#humidity");
 var currentWSpeed = $("#wind-speed");
-var currentUvindex = $("#UVindex");
-var inputValue = document.querySelector(".inputValue")
-var storage=window.localStorage;
+var currentUvindex = $("#uvindex");
+var storage = window.localStorage;
 
+//var url = `https://api.openweathermap.org/data/2.5/weather?q={inputValue}&appid={apiKey}`;
 var weatherCard = document.querySelector("#current-weather");
-var forecastCard = document.querySelector("#forecastContainer");
+var forecastCard = document.querySelector(".forecast");
 
-//function getWeather(city){
-searchBtn.on("click", function() {
-    fetch = ("https://api.openweathermap.org/data/2.5/weather?q="+inputValue.value+"&appid=70bfb4227a0a636df084027cc1991ebc")
-    .then(response => response.json())
-    .then(data => console.log(data))
-})
+$(document).ready(function(){
+    var cities = [];
+//call weather api 
+    function displayCurrentWeather(city) {
+        var apiKey = "70bfb4227a0a636df084027cc1991ebc";
+        var url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
 
-//WHEN I search for a city
-//THEN I am presented with current and future conditions for that city and that city is added to the search history
-function displayWeather(event){
-    event.preventDefault();
-    if(search.val().trim()!==""){
-        city=search.val().trim();
-        currentWeather(city);
+        $.ajax({
+            url: url,
+            method: "GET"
+        }).then(function(response){
+            var forecastIcon = response.weather[0].symbol;
+            var date = $("<h2>").text(moment().format('l'));
+            var symbol = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + forecastIcon + "@2x.png"); 
+            var temperature = (response.main.temp - 273.15) * 1.80 + 32;
+            //display current weather conditions
+            $("#current-city").append(response.name);
+            $("#current-date").append(date);
+            $("#forecastIcon").append(symbol);
+            $("#temperature").apend(temperature, " \u00B0F");
+            $("#humidity").append(response.main.humidity + "%");
+            $("#wind-speed").append(response.wind.speed + "MPH");
+            //UV index one-call
+            var lat = response.coord.lat
+            var long = response.coord.long
+            url = "https://api.openweathermap.org/data/2.5/onecall?lat=" lat + "&lon=" + long + "&exclude=" + part + "&appid=" + apiKey; 
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function(response){
+            var uvIndex = response.value;
+            $("#uvindex").append(response.value);
+        });
+    });
+
+//event listeners
+$("#searchBtn").click(function() {
+    var searches = 
+
+        }
+        localStorage.setItem(text);
     }
-function currentWeather() {
-    
-}
-//WHEN I view current weather conditions for that city
-//THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-
-//WHEN I view the UV index
-//THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-
-//WHEN I view future weather conditions for that city
-//THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-
-
-
+)}})
+//local storage 
+$(".input-group #search-city").val(localStorage.getItem("#search-city"));
+//call the function
